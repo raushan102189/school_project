892cef77bc7dc9aacb2766226b110bc999d3a78e
brandon <raushan102189@gmail.com>
2023-01-17 03:21:06 +0530

    read txt file and # in the word
    Read a text file line by line and display each word separated by a #

---
+++ read.py
@@ -0,0 +1,10 @@
+with open("computer_science.txt", "r") as file:
+    reading = file.readlines()
+print(len(reading))
+for i in range(1, len(reading)):
+    listword =reading[i].split()
+    for j in range(0, len(listword)):
+        print("#"+ listword[j])
+   
+    print(listword)
+    print("@"*100)
